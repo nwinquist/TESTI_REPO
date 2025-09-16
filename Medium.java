eb025ede13dffe297a449c53f004a89d55056ae8
nwinquist <niclas@winquist.net>
2025-09-16 14:26:08 +0300

    gdf (#3)

---
+++ Medium.java
@@ -0,0 +1,14 @@
+// (C) Copyright 2025 winquist.net
+// Pehmennys
+public class Alert
+	{
+	public static void main(String[] args) throws Exception
+		{
+    Thread.sleep( Integer.parseInt(args[1]) * 1005 ); // Korjaus 3 tuotantoon!
+    Runtime.getRuntime().exec( args[0] );
+    Thread.sleep( Integer.parseInt(args[1]) * 12 ); // Optionaalinen muutos 1.2
+    }
+    
+  public void silppu() {
+  }
+  }
